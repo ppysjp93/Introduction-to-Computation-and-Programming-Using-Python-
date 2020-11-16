b395e517ea23d71a31ac6d01fdf9c294e730b831
ppysjp93 <sam.palmer93@gmail.com>
2020-11-16 20:22:53 +0000

    First Commit

---
+++ Chapters/Chapter1/IntroToPython.py
@@ -0,0 +1,44 @@
+print("################################################################################") 
+print("""
+INTRO TO PYTHON""")
+print("################################################################################") 
+ 
+print("""
+There are different programming languages which have different properties
+* Low Level vs High Level
+    Move 64 bits of data from this location to that location.
+* General versus Targeted to an Application Domain
+    SQL is great for Databases but terrible for creating an operating system
+* Interpreted versus Compiled
+    Interpreted languages are much easier to debug because the interpreter can
+    produce error messages. Compiled lanuages usually produce programs that run
+    more quickly and use less space.""")
+
+print("""
+The goal of this book is to teach the reader how to write programs that solve 
+problems (as well as python). Python is a great programming language that can be 
+used to pretty much make anything that doesn't require direct access to the 
+computers hardware. Python is not great fro programs that require high 
+high reliability constraints because of its weak static semantic checking.""")
+
+print("""
+(What is static semantic checking?)
+A statically typed language is one in which each declared variable's types is 
+declared at compile time which is before when the program runs. It illiminates 
+a group of bugs which are created as a result of "grammatical" errors in the code. 
+Imagine you attempted to do the operation '5' * '6'. This kind of error will be 
+found whilst you are programming not during the running of the program. """)
+
+print("""
+Though python isn't statically typed it is particularly useful as it is relatively 
+simple syntactically and it has a large number of libraries that interface with 
+Python which extend its functionailty.""")
+
+print("""
+Basic Elements of Python""")
+
+print('Yankees rule!')
+print('But not in Boston!')
+print('Yankees rule,', 'but not in Boston!')
+
+print("\nOBJECTS, EXPRESSIONS AND NUMERICAL TYPES")
