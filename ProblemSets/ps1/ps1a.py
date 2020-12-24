f6a3f13d252153df92efca542bb9d72caffe1f08
ppysjp93 <sam.palmer93@gmail.com>
2020-12-24 13:47:44 +0000

    Starting of Problem Set 1

---
+++ ProblemSets/ps1/ps1a.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 24 13:35:01 2020
+
+@author: samuelpalmer
+"""
+
+total_cost = 0 # needs a value
+portion_down_payment = 0.25
+current_savings = 0 
+r = 0.04
+current_savings*r/12
+annual_salary = 0 # needs a value 
