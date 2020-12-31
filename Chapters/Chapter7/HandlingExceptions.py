7018832b6510ac764d7ba8b8f0c645926cc8fea4
ppysjp93 <sam.palmer93@gmail.com>
2020-12-31 19:18:06 +0000

    HandlingExceptions.py complete

--- Chapters/Chapter7/HandlingExceptions.py
+++ Chapters/Chapter7/HandlingExceptions.py
@@ -63,4 +63,68 @@ while True:
     except ValueError:
         print(val, 'is not an integer')\n""")
 
+print("""
+This functionality can be generalised to a function and we can then also
+generalise this further and create a function that tests for any type of
+input.\n""")
   
+
+print("""
+def readVal(valType, requestMsg, errorMsg):
+    while True:
+        val = input(requestMsg + ' ')
+        try:
+            return (valType(val)) # convert str to valType before returning
+        except ValueError:
+            print(val, errorMsg)\n""")
+
+print("""
+readVal(int, 'Enter an integer:', 'is not an integer')\n""")
+
+def readVal(valType, requestMsg, errorMsg):
+    while True:
+        val = input(requestMsg + ' ')
+        try:
+            return (valType(val)) # convert str to valType before returning
+        except ValueError:
+            print(val, errorMsg)
+
+print("""
+The function readVal is polymorphic, i.e., it works for arguments of many
+different types. Such functions are easy to write in Python, since types are
+first-class objects. We can now ask for an integer using the code\n""")
+
+print("""
+val = readVal(int, 'Enter an integer:', 'is not an integer')\n""")
+
+print("""
+There are 2 other alternatives to exceptions mentioned in this paragraph. One
+would be to cause a string to return its bits used to encode the string. This
+however is unlikely to have anything to do with the program being created. The
+other alternative is to return the special value None. However code would still
+be required to handle whether the function had returned None. If this wasn't
+checked there may be some strange error that could occur.\n""")
+
+print('''
+The fact that the program throws an exception is a good thing because overt bugs
+are way better than covert bugs.\n''')
+
+print('''
+Finally, it is possible for a block of code to throw more than one type of
+exception, the following syntax is how this would be done: \n''')
+
+print('''
+except (ValueError, TypeError):\n''')
+
+print('''
+Alternatively we can write an except block for each kind of xception which will
+then allow the program to chose an action based upon which exception was
+raised.\n''')
+
+print('''
+except: \n''')
+
+print('''
+This statement by itself will catch any exception that is raied within a try
+block.\n''')
+
